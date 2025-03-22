478577c327e34c3f10ad30bf94a714121050716d
André Yermak Naumenko <asyermak@gmail.com>
2025-03-22 14:50:09 +0100

    PacketListener-ProtocolLib

--- src/main/java/net/ezplace/groupChat/listeners/PacketListener.java
+++ src/main/java/net/ezplace/groupChat/listeners/PacketListener.java
@@ -1,4 +1,37 @@
 package net.ezplace.groupChat.listeners;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import net.ezplace.groupChat.GroupChat;
+import org.bukkit.entity.Player;
+
 public class PacketListener {
+    GroupChat plugin = GroupChat.getInstance(); //Va a dar null pointer exception?
+    public void register() {
+        ProtocolLibrary.getProtocolManager().addPacketListener(
+                new PacketAdapter(plugin, ListenerPriority.NORMAL,
+                        PacketType.Play.Server.CHAT,
+                        PacketType.Play.Server.TITLE) {
+
+                    @Override
+                    public void onPacketSending(PacketEvent event) {
+                        if (shouldTranslate(event.getPlayer())) {
+                            event.setCancelled(true);
+                            resendTranslated(event);
+                        }
+                    }
+                });
+    }
+
+    public boolean shouldTranslate(Player eventPlayer){
+        //logica a implementar para traducir
+        return false;
+    }
+
+    public void resendTranslated(PacketEvent event){
+        //logica para reenviar el nuevo mensaje;
+    }
 }
